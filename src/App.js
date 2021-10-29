9030bb92039792d7b2f8dd150164443a1793d9c3
samirabis <skynordp@gmail.com>
2021-10-29 12:39:47 +0200

    main commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+import { Proportion } from "./components/Proportion";
+
+function App() {
+  return (
+    <div className="App">
+      <Proportion />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Proportion.js
@@ -0,0 +1,186 @@
+import React from "react";
+import { Component } from "react";
+import Navbar from "./Navbar";
+import * as _ from "lodash";
+
+export class Proportion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newIngredient: "",
+      newQuantity: "",
+      newType: "gr",
+      items: [],
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.addNewItem = this.addNewItem.bind(this);
+    this.removeItem = this.removeItem.bind(this);
+    this.changeQuantity = this.changeQuantity.bind(this);
+  }
+
+  addNewItem() {
+    const newItem = {
+      id: _.uniqueId(),
+      ingredient: this.state.newIngredient,
+      quantity: parseFloat(this.state.newQuantity),
+      type: this.state.newType,
+      newQuantity: 0,
+    };
+
+    const items = [...this.state.items];
+
+    if (items.length) {
+      const quantity = items[0].quantity;
+      const newQuantity = items[0].newQuantity;
+      newItem.newQuantity = newItem.quantity * (newQuantity / quantity);
+    }
+
+    items.push(newItem);
+
+    this.setState({
+      newIngredient: "",
+      newQuantity: "",
+      newType: "gr",
+      items: items,
+    });
+  }
+
+  removeItem(id) {
+    const index = this.state.items.findIndex((item) => {
+      return item.id === id;
+    });
+
+    const items = [...this.state.items];
+    items.splice(index, 1);
+
+    this.setState({
+      items,
+    });
+  }
+
+  handleChange(e) {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  changeQuantity(e, id) {
+    const value = parseFloat(e.target.value);
+    const items = [...this.state.items];
+    const item = items.find((item) => item.id === id);
+    const newItem = item;
+    newItem.newQuantity = value;
+
+    for (let i = 0; i < items.length; i++) {
+      const currentItem = items[i];
+      if (currentItem.id === id) {
+        continue;
+      }
+
+      currentItem.newQuantity = (
+        currentItem.quantity *
+        (newItem.newQuantity / newItem.quantity)
+      ).toFixed(2);
+    }
+
+    this.setState({
+      items,
+    });
+  }
+
+  render() {
+    return (
+      <div className="container mx-auto">
+        <Navbar />
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            <thead>
+              <tr>
+                <th>Ingrediente</th>
+                <th>Quantità</th>
+                <th>Tipo</th>
+                <th>Nuova Quantità</th>
+              </tr>
+            </thead>
+            {this.state.items.map((item) => (
+              <tbody key={item.id}>
+                <tr>
+                  <td>{item.ingredient}</td>
+                  <td>{item.quantity}</td>
+                  <td>{item.type}</td>
+                  <td>
+                    <input
+                      placeholder="Nuova quantità"
+                      className="input has-text-danger"
+                      name="editedQuantity"
+                      type="number"
+                      value={item.newQuantity}
+                      onChange={(e) => this.changeQuantity(e, item.id)}
+                    />
+                  </td>
+                  <td className="column">
+                    <button
+                      className="btn btn-secondary"
+                      onClick={(e) => this.removeItem(item.id)}
+                    >
+                      Rimuovi
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            ))}
+          </table>
+        </div>
+        <div className="container mx-auto py-10">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Ingrediente</span>
+            </label>
+            <input
+              placeholder=""
+              className="input input-bordered"
+              type="text"
+              name="newIngredient"
+              value={this.state.newIngredient}
+              onChange={this.handleChange}
+            />
+            <label className="label">
+              <span className="label-text">Quantità</span>
+            </label>
+            <input
+              placeholder="Quantità"
+              className="input input-bordered"
+              type="number"
+              name="newQuantity"
+              value={this.state.newQuantity}
+              onChange={this.handleChange}
+            />
+            <label className="label">
+              <span className="label-text">Tipo</span>
+            </label>
+            <select
+              className="select select-bordered w-full max-w-ws"
+              name="newType"
+              value={this.state.newType}
+              onChange={this.handleChange}
+            >
+              <option selected value="gr">
+                grammi
+              </option>
+              <option value="ml">millilitri</option>
+              <option value="kg">chili</option>
+              <option value="pz">porzioni</option>
+            </select>
+            <button className="btn btn-primary" onClick={this.addNewItem}>
+              Aggiungi
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
